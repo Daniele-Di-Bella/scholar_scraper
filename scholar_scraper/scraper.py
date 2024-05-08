729bdf706f7e133026fae4e416c48af8bb7f7647
Daniele Di Bella <daniele.dibella@awi.de>
2024-05-08 12:47:37 +0200

    encoding added

--- scholar_scraper/scraper.py
+++ scholar_scraper/scraper.py
@@ -79,7 +79,7 @@ def scrape(keywords, num_pages, most_recent):
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     field_names = ["Score", "Title", "Link"]
-    with open(os.path.join(current_dir, "papers.csv"), "w") as file:
+    with open(os.path.join(current_dir, "papers.csv"), "w", encoding="UTF-8") as file:
         writer = csv.DictWriter(file, fieldnames=field_names)
         writer.writeheader()
         writer.writerows(papers)
